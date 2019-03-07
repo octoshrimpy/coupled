e40ac7c43938021dccf48477fdeb4d24c91d6da0
octoshrimpy <octavo.acapella@gmail.com>
2019-03-07 15:41:04 -0700

    sets up project, sets up buefy

---
+++ vue.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  pwa: {
+    themeColor: '#482778'
+  },
+
+  publicPath: undefined,
+  outputDir: undefined,
+  assetsDir: undefined,
+  runtimeCompiler: undefined,
+  productionSourceMap: undefined,
+  parallel: undefined,
+
+  css: {
+    sourceMap: true
+  }
+}
